b8a84e76ff9159f330167571a2a203963408ea9f
JACOB OLUWAFEMI <167685170+Holuwaphemmy01@users.noreply.github.com>
2025-10-12 20:03:49 +0100

    firts commit and adk ts working

---
+++ index.ts
@@ -0,0 +1,16 @@
+// src/agents/index.ts
+import { agent } from "./src/agents/example.ts";
+import dotenv from "dotenv";
+
+dotenv.config();
+console.log("✅ Loaded GOOGLE_API_KEY:", process.env.GOOGLE_API_KEY ? "FOUND" : "MISSING");
+
+
+async function main() {
+    const { runner } = await agent();
+
+    const response = await runner.ask("What is the capital of France?");
+    console.log("🤖 Response:", response);
+}
+
+main().catch(console.error);
