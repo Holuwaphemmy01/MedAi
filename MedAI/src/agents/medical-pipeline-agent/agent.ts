3c5a47ef76a006160296cba4c04adb01440f85d6
JACOB OLUWAFEMI <167685170+Holuwaphemmy01@users.noreply.github.com>
2025-10-20 19:14:02 +0100

    medical pipeline

--- MedAI/src/agents/medical-pipeline-agent/agent.ts
+++ MedAI/src/agents/medical-pipeline-agent/agent.ts
@@ -1,14 +1,16 @@
-// import { LlmAgent } from "@iqai/adk";
+import { LlmAgent, SequentialAgent } from "@iqai/adk";
 // import { env } from "../../env";
+import { getQuestionFromUserAgent } from "../question-asker-agent/agent";
+import { getSymptomAnalyzerAgent } from "../symptom-analyzer-agent/agent";
+import { getRecommendationAgent } from "../recommendation-agent/agent";
 
 
-// export const getQuestionFromUserAgent = () => {
-//     const questionAgent = new LlmAgent({
-//         name: "question_agent",
-//         description: "Responsible for gathering all relevant patient information through structured and adaptive questioning. It collects key data such as age, gender, symptom descriptions, duration, severity, and relevant medical history. The agent ensures completeness and accuracy of responses, reformats them into a structured format, and prepares the data for downstream analysis by other agents.",
-//         model: env.LLM_MODEL,
-//         tools: [],
-//     });
+const getMedicalPipelineAgent = new SequentialAgent({
+  name: "medical-pipeline-research-agent",
+  description: "Comprehensive market analysis and strategy development",
+  subAgents: [getQuestionFromUserAgent(), getSymptomAnalyzerAgent(), getRecommendationAgent()],
+});
 
-//     return questionAgent;
-// };
+
+
+
